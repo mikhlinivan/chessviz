2d18e0a9ee89341c02adfbcc006d96814a51c3a9
Михлин Иван Андреевич <is741s17@mwp06.csc.local>
2018-03-01 08:35:36 +0700

    реализация вывода начального положения доски

---
+++ chessviz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+    {
+    printf("  a b c d e f g h|\n");
+    printf("8|Л|К|С|Ф|К|С|К|Л|8\n");
+    printf("7|П|П|П|П|П|П|П|П|7\n");
+    printf("6| |#| |#| |#| |#|6\n");
+    printf("5|#| |#| |#| |#| |5\n");
+    printf("4| |#| |#| |#| | |4\n");
+    printf("3|#| |#| |#| |#| |3\n");
+    printf("2|п|п|п|п|п|п|п|п|2\n");
+    printf("1|л|к|с|ф|к|с|к|л|1\n");
+    printf("  a b c d e f g h \n");
+    }
